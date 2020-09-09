18d6b9bd9853224349b685ab3163e25a19d3b264
Pepe Valdivia <jvaldivia@softweb.pe>
2020-09-09 17:51:32 -0500

    enviando un post

--- app.py
+++ app.py
@@ -4,26 +4,34 @@
 from flask import Flask, request
 
 APP = Flask(
-  __name__,
-  static_folder='static',
-  static_url_path='/',
+    __name__,
+    static_folder='static',
+    static_url_path='/',
 )
 
 @APP.route('/')
 def home():
     return 'Hello, World?'
 
-@APP.route('/demo_path/<name>/<int:age>')
+@APP.route('/demo_path/<name>/<int:age>', methods=['GET'])
 def demo_path(name, age):
     print('path parameter -> nombre : %s, edad : %d' % (name, age))
     return 'path parameter -> nombre : %s, edad : %d' % (name, age)
 
-@APP.route('/demo_query')
+@APP.route('/demo_query', methods=['GET'])
 def demo_query():
     name = request.args.get('name')
     age = int(request.args.get('age'))
     print('query parameter -> nombre : %s, edad : %d' % (name, age))
     return 'query parameter -> nombre : %s, edad : %d' % (name, age)
+
+@APP.route('/demo_post', methods=['POST'])
+def demo_post():
+    name = request.form['name']
+    age = int(request.form['age'])
+    print(request.method)
+    print('post parameter -> nombre : %s, edad : %d' % (name, age))
+    return 'post parameter -> nombre : %s, edad : %d' % (name, age)
 
 if __name__ == '__main__':
     APP.run(
