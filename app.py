78b098eb2d4329319c727810305c1686100c2ee1
Pepe Valdivia <jvaldivia@softweb.pe>
2020-09-10 12:21:52 -0500

    status code

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ APP = Flask(
 
 @APP.route('/')
 def home():
-    return 'Hello, World?'
+    return 'Hello, World?', 400
 
 @APP.route('/demo_path/<name>/<int:age>', methods=['GET'])
 def demo_path(name, age):
     print('path parameter -> nombre : %s, edad : %d' % (name, age))
-    return 'path parameter -> nombre : %s, edad : %d' % (name, age)
+    return 'path parameter -> nombre : %s, edad : %d' % (name, age), 200
 
 @APP.route('/demo_query', methods=['GET'])
 def demo_query():
     name = request.args.get('name')
     age = int(request.args.get('age'))
     print('query parameter -> nombre : %s, edad : %d' % (name, age))
-    return 'query parameter -> nombre : %s, edad : %d' % (name, age)
+    return 'query parameter -> nombre : %s, edad : %d' % (name, age), 200
 
 @APP.route('/demo_post', methods=['POST'])
 def demo_post():
@@ -31,7 +31,7 @@ def demo_post():
     age = int(request.form['age'])
     print(request.method)
     print('post parameter -> nombre : %s, edad : %d' % (name, age))
-    return 'post parameter -> nombre : %s, edad : %d' % (name, age)
+    return 'post parameter -> nombre : %s, edad : %d' % (name, age), 200
 
 if __name__ == '__main__':
     APP.run(
